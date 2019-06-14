dadec20bae07b4c1624dc778c759c240ad9fdb5b
Spafbi <chris.snow@spafbi.com>
2019-06-14 15:25:55 -0500

    Adding new files

---
+++ info/Scripts/Entities/Events/AirDropCrate.lua
@@ -0,0 +1,18 @@
+CreateItemTable("AirDropCrate");
+
+-- Called when the crate is initially dropped
+function AirDropCrate:OnDropped()
+	--Log("AirDropCrate - OnDropped")
+end
+
+-- Called when the crate is close to landing - about 10m above the ground
+-- This is when the crate is physicalized and falls due to gravity
+function AirDropCrate:OnLanding()
+	--Log("AirDropCrate - OnLanding")
+end
+
+-- Called when the crate is about to land - about 2.5m above the ground
+-- This is when the lading particle effects and audio are played
+function AirDropCrate:OnLanded()
+	--Log("AirDropCrate - OnLanded")
+end
